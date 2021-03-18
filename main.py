7d4a334d65d29b4691542ca2ca30554db95b8795
Zet Truong <dungtq@aipower.com.vn>
2021-03-18 19:36:58 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,175 @@
+import pygame
+import random
+
+pygame.font.init()
+
+# Constant
+s_width = 800
+s_height = 700
+block_size = 30
+play_width = 300  # mean 10 block
+play_height = 600  # mean 20 block
+
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height
+
+# Shape Format
+S = [['.....',
+      '.....',
+      '..00.',
+      '.00..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['..0..',
+      '..0..',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '0000.',
+      '.....',
+      '.....',
+      '.....']]
+
+O = [['.....',
+      '.....',
+      '.00..',
+      '.00..',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+shapes = [S, Z, I, O, J, L, T]
+shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+
+
+class Piece(object):
+    pass
+
+
+def create_grid():
+    pass
+
+
+def convert_shape_format(shape):
+    pass
+
+
+def valid_space(shape, grid):
+    pass
+
+
+def check_lost(positions):
+    pass
+
+
+def get_shape():
+    pass
+
+
+def draw_text_middle(text, size, color, surface):
+    pass
+
+
+def draw_grid(surface, row, col):
+    pass
+
+
+def clear_rows(grid, locked):
+    pass
+
+
+def draw_next_shape(shape, surface):
+    pass
+
+
+def draw_window(surface):
+    pass
+
+
+def main():
+    pass
+
+
+def main_menu():
+    pass
+
+
+main_menu()  # start game
